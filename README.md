63378c282dd9d29aa260441d11435fa12a97aa96
safe6Sec <1850597152@qq.com>
2021-07-25 22:19:59 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# ShiroAndFastJson
+
+shiro加fastjson环境
+
+
+
+路由：
+
+/login 登录
+
+/json json解析
---
+++ src/main/java/com/shiro/vuln/Controller/IndexController.java
@@ -0,0 +1,36 @@
+package com.shiro.vuln.Controller;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.parser.ParserConfig;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+
+    @PostMapping("/json")
+    @ResponseBody
+    public JSONObject parse(@RequestBody String data){
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("status",1);
+        //ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        //com.sun.org.apache.bcel.internal.util.ClassLoader；
+       // com.sun.org.apache.bcel.internal.util.ClassPath
+
+        // java.awt.Point
+
+
+
+        if (JSON.parseObject(data)!=null){
+            jsonObject.put("status",0);
+            return jsonObject;
+        }
+        return jsonObject;
+    }
+}
